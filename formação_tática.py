c02ed6b418a2cdd77dce11399df9334bc30c05b1
Cristiano Silva <cris_cmfs5@hotmail.com>
2023-07-17 20:00:16 +0100

    Com update do plantel

--- formação_tática.py
+++ formação_tática.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import random
-import subprocess
+from tkinter import * # Para poder utilizar a linha de apagar a entrada de texto
+import pandas as pd # para guardar os dados dos jogadores
 
 # Configurações do campo de futebol
 comprimento_campo = 900
@@ -90,6 +91,15 @@ jogar_button.grid(row=52, column=0)
 # Chamada inicial para desenhar o campo de futebol
 desenhar_campo()
 
+#####################################################################
+#                                                                   #
+# ------------ Secção de Código para Gestão do Plantel ------------ #
+#                                                                   #
+#####################################################################
+
+saldo = 0
+
+
 ## Conjunto de Widgets para adicionar jogador
 nome_label = tk.Label(janela, text="Nome do Jogador")
 nome_label.grid(row=0,column=2)
@@ -107,5 +117,116 @@ posicao.insert(2, "Médio")
 posicao.insert(3, "Defesa")
 posicao.insert(4, "Gaurda-Redes")
 posicao.grid(row=3,column=3)
+
+plantel_list = tk.Listbox(janela, width=20, height=10)
+plantel_list.grid(row=4, column=3, rowspan=2)
+scrollbar = Scrollbar(janela)
+scrollbar.grid(row=4, column=4, rowspan=2, sticky="ns")
+plantel_list.config(yscrollcommand = scrollbar.set)
+scrollbar.config(command = plantel_list.yview)
+
+jogadores_dict = {
+    "Nome Jogador":[],
+    "Idade":[],
+    "Posição":[],
+    "Velocidade": [],
+    "Força":[],
+    "Resistência":[],
+    "Drible":[],
+    "Passe":[]
+    }
+jogadores_df = pd.DataFrame(jogadores_dict)
+
+# Para adicionar um jogador ao plantel a partir dos inputs
+def command_adicionar_dados_jogador():
+    dados_jogador = [
+        nome.get(), 
+        idade.get(), 
+        posicao.get(posicao.curselection()), 
+        random.randint(1,10), 
+        random.randint(1,10), 
+        random.randint(1,10),
+        random.randint(1,10),
+        random.randint(1,10)
+        ]
+    jogadores_df.loc[len(jogadores_df)] = dados_jogador
+    inserir_na_lista = f"{dados_jogador[0]}" # Insere o nome do jogador na lista com base no número do jogador na tabela de dados
+    plantel_list.insert(END, inserir_na_lista) #Adicionar o nome do jogador à lista
+    nome.delete(0, END)
+    idade.delete(0, END)
+    posicao.selection_clear(0, "end")
+    return
+
+botao_submeter = tk.Button(janela, text="Adicionar jogador", command=command_adicionar_dados_jogador)
+botao_submeter.grid(row=4, column=2)
+
+# Excerto para criar plantel aleatório
+exemplos_primeiro_nome = ["David","Cristiano","João", "Pedro", "Alberto", "Inácio", "Mauro", "José", "Eusébio", "Ricardo", "Miguel"]
+exemplos_apelido = ["Rebelo","Rocha","Silva", "Pereira", "Marques", "Santos", "Parreira", "Tavares", "Sousa"]
+for i in range(11):
+    dados_jogador = [ # os nomes não se podem repetir se não os searchs ficam todos baralhados... Tenho que conseguir extrair o indice unico de cada jogador
+        random.choice(exemplos_primeiro_nome)+" "+random.choice(exemplos_apelido), 
+        random.randint(18,34), 
+        random.choice(["Atacante", "Médio", "Defesa", "Guarda-Redes"]),
+        random.randint(1,10), 
+        random.randint(1,10), 
+        random.randint(1,10),
+        random.randint(1,10),
+        random.randint(1,10)
+        ]
+    jogadores_df.loc[len(jogadores_df)] = dados_jogador
+    inserir_na_lista = f"{dados_jogador[0]}"
+    plantel_list.insert(END, inserir_na_lista) #Adicionar o nome do jogador à lista
+
+#----------------------------------#
+#-- Menu de Treino dos jogadores --#
+#----------------------------------#
+    def detalhes_jogador():
+        global saldo
+        nome_jogador = plantel_list.get(plantel_list.curselection())
+        detalhes_app = Toplevel(janela)
+        detalhes_app.title("Estatísticas Jogador")
+        detalhes_app.geometry("500x500")
+        #print(jogadores_df[jogadores_df["Nome Jogador"]==nome_jogador].get(key = "Idade"))
+        label_nome_jogador = tk.Label(detalhes_app, text=f"Nome: {nome_jogador}")
+        label_nome_jogador.pack()
+        idade_jogador = jogadores_df[jogadores_df["Nome Jogador"]==nome_jogador]["Idade"].values[0]
+        label_idade = tk.Label(detalhes_app, text=f"Idade: {idade_jogador}")
+        label_idade.pack()
+        posicao_jogador = jogadores_df[jogadores_df["Nome Jogador"]==nome_jogador]["Posição"].values[0]
+        label_posicao = tk.Label(detalhes_app, text=f"Posição: {posicao_jogador}")
+        label_posicao.pack()
+        stat1_jogador = jogadores_df[jogadores_df["Nome Jogador"]==nome_jogador]["Velocidade"].values[0]
+        label_stat1 = tk.Label(detalhes_app, text=f"Velocidade: {stat1_jogador}")
+        label_stat1.pack()
+        stat2_jogador = jogadores_df[jogadores_df["Nome Jogador"]==nome_jogador]["Força"].values[0]
+        label_stat2 = tk.Label(detalhes_app, text=f"Força: {stat2_jogador}")
+        label_stat2.pack()
+        stat3_jogador = jogadores_df[jogadores_df["Nome Jogador"]==nome_jogador]["Resistência"].values[0]
+        label_stat3 = tk.Label(detalhes_app, text=f"Resistência: {stat3_jogador}")
+        label_stat3.pack()
+        stat4_jogador = jogadores_df[jogadores_df["Nome Jogador"]==nome_jogador]["Drible"].values[0]
+        label_stat4 = tk.Label(detalhes_app, text=f"Drible: {stat4_jogador}")
+        label_stat4.pack()
+        stat5_jogador = jogadores_df[jogadores_df["Nome Jogador"]==nome_jogador]["Passe"].values[0]
+        label_stat5 = tk.Label(detalhes_app, text=f"Passe: {stat5_jogador}")
+        label_stat5.pack()
+
+        #BOTÕES PARA TREINAR
+        def command_treinar():
+            old_value = jogadores_df[jogadores_df["Nome Jogador"]==nome_jogador]["Velocidade"].values[0]
+            print(old_value)
+            new_value = old_value + 1
+            print(new_value)
+            teste = jogadores_df.xs("Velocidade")
+            print(teste)
+            return
+        
+        botao_treino1 = tk.Button(detalhes_app, text="Treino1", command=command_treinar)
+        botao_treino1.pack()
+        return
+## botão para selecionar jogador e abrir menu de treino
+botao_selecionar = tk.Button(janela, text="Detalhes", command=detalhes_jogador)
+botao_selecionar.grid(row=5,column=2)
 
 janela.mainloop()
